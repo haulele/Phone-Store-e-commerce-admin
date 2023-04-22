76861cca73de7490a4e854548d46b60a41d6a5c8
haulele <20520493@gm.uit.edu.vn>
2023-04-22 23:06:58 +0700

    add simple layout

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+
 
 export default function Home() {
-  const { data: session } = useSession();
-  if(!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-        </div>
-      </div>
-    );
-  }
-  return (
-    <div>logged in {session.user.email}</div>
-  )
+  const {data: session} = useSession();
+  return <Layout>
+    <div>
+      Hello, {session?.user?.email}
+    </div>
+  </Layout>
 }
